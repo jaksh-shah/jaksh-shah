2e8981fcd2c213b5a3ae2021b6792a466c460832
The goated kid <947119@pdsb.net>
2024-11-18 19:46:03 -0500

    Add files via upload

---
+++ 10.CW.py
@@ -0,0 +1,73 @@
+##i = 10
+##while i > 2:
+##        print(i)
+##        i = i - 3
+##
+##for i in range(10,2,-3):
+##    print(i)
+
+
+##print("Hello", end="")
+##print("World!", end="")
+##for i in range(10):
+##    print(i, end="\t")
+##for i in range(5):
+##    print(i, end="0\n")
+
+
+##user_name = "Gurleen"
+##user_age = 13
+##name = "Kyle"
+##print("%s is your name, and %s is mine."%(user_name,name))
+##print("Username:%s, age:%d years old."%(user_name, user_age))
+
+
+##for i in range(10):
+##    for j in range(5): 
+##        print("*", end="\t")
+##    print()
+
+    
+##for x in range(8):
+##    for char in "abcd": 
+##        print(char, end=" ")
+##    print(“----”)
+
+   
+##for a in range(1,6):
+##    for b in range(a):
+##        print("#", end="")
+##    print()
+
+##for i in range(5):
+##    for j in range(5):
+##        print(i * j, end="\t")
+##    print()
+
+#============================Class Questions===================================
+#1.
+##print("****\n****\n****\n****")
+
+#2.
+##print("****\n****\n****\n****")
+##more = int(input("Would you like more stars?"))
+##if more == "yes":
+##    print("****\n****\n****\n****")
+##    more = int(input("Would you like more stars?"))
+
+#3.
+name = input("What is your name?")
+for i in range(3):
+    for j in range(5):
+        print(name,end = " ")
+    print()
+#4
+##for i in range(1,11):
+##     for j in range(1,11):
+##          print(i * j, end="\t")
+
+#5.
+##print("             |             |             \n---------------------------------------\n             |             |             \n--------------------------------------\n             |             |             ")
+
+#6.
+##print("----------------------\n|       |       |      |\n----------------------\n|       |       |       |\n----------------------\n|       |       |      |\n----------------------")
